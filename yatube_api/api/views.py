ab5cb1b6cd4e2e5cd33c6ecf378adc66ee218275
useyourcode <154615654+useyourcode@users.noreply.github.com>
2024-07-02 22:41:02 +0300

    v1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, viewsets
 from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly
+)
 
 from posts.models import Comment, Follow, Group, Post
 from .permissions import IsAuthorOrReadOnly
